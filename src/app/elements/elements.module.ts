a0eed0a79e2278676038d930eeb5105a11e462cc
miha-9111 <miha-9111@yandex.ru>
2022-01-06 05:03:50 +0300

    Importing and Exporting Modules

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ElementsHomeComponent
+  ],
+  exports: [
+    ElementsHomeComponent
+  ],
   imports: [
     CommonModule,
     ElementsRoutingModule
