fbafe518fb069953110cd02a5fefa9b3389014a1
miha-9111 <miha-9111@yandex.ru>
2022-01-06 22:05:58 +0300

    Navigating with RouterLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from "./elements/elements.module";
-import {CollectionsModule} from "./collections/collections.module";
+import { CollectionsModule } from "./collections/collections.module";
 
 @NgModule({
   declarations: [
