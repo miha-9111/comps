942e9f89dab05c2d23f3a94e51a96f9a24406550
miha-9111 <miha-9111@yandex.ru>
2022-01-11 03:14:57 +0300

    Displaying a List of Items

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -5,12 +5,14 @@ import { SharedModule } from "../shared/shared.module";
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
   declarations: [
     ViewsHomeComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
